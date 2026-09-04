136e06cf51c7b2592b9c0c516aa7550ace9d8ffb
thiagonerieu <thiagonerieu@gmail.com>
2026-09-04 09:47:12 +0200

    FEAT : les variables d’environnement pour la connexion à la BDD .

---
+++ env.php
@@ -0,0 +1,14 @@
+<?php
+
+//- $_ENV['dbhost'] avec la valeur localhost
+//- $_ENV['dbname'] avec le nom de la base de données (supergame)
+//- $_ENV['login'] avec votre login de connexion au serveur MySQL
+//- $_ENV['password'] avec votre mot de passe de connexion au serveur MySQL
+
+//Identifiant de connexion à la BDD
+$_ENV['dbhost'] = 'localhost';
+$_ENV['dbname'] = "supergame";
+$_ENV['dblogin'] = 'root';
+$_ENV['dbpassword'] = "root";
+
+?>
